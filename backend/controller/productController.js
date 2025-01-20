e0ac65fefbc92dcf4347f0de0e9612b4143bb2c5
optanwar <optanwar10@gmail.com>
2025-01-21 03:00:22 +0530

    add error middleware

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,85 +1,75 @@
 const Product = require("../module/productModule");
 const ErrorHandler = require("../utils/errorHandler");
 
-
-
-
 // Create Products --Admin
-exports.createProduct =async ( req, res, next ) =>{
-    const product = await Product.create(req.body);
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
 
-    res.status(201).json({
-        success: true,
-        message: "Product created successfully",
-        product
-    })
-}
-
+  res.status(201).json({
+    success: true,
+    message: "Product created successfully",
+    product,
+  });
+};
 
 // Get all Products
 
-exports.getAllProducts = async (req, res, next)=>{
-    const products = await Product.find();
+exports.getAllProducts = async (req, res, next) => {
+  const products = await Product.find();
 
-    res.status(200).json({
-        success: true,
-        message: "Products retrieved successfully",
-        products
-    })
-}
-
+  res.status(200).json({
+    success: true,
+    message: "Products retrieved successfully",
+    products,
+  });
+};
 
 // Get Products details
 exports.getProductDetails = async (req, res, next) => {
-    const product = await Product.findById(req.params.id);
-    if(!product) {
-        return next(new ErrorHandler("Product not found",404));
-}
+  const product = await Product.findById(req.params.id);
 
-res.status(200).json({
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+
+  res.status(200).json({
     success: true,
     message: "Product details retrieved successfully",
-    product
-})
-}
+    product,
+  });
+};
 
 // Update product --Admin
 
-exports.updateProduct = async (req, res, next) =>{
-    let product = await Product.findById(req.params.id);
-   
-    if(!product){
-        return res.status(500).json({
-            success: false,
-            message: "Product not found"
-        })
-    }
-    product = await Product.findByIdAndUpdate(req.params.id, req.body,  {
-        new: true,
-        runValidators: true
-      });
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
 
-    res.status(200).json({
-        success: true, 
-        message: "Product updated successfully",
-        product})
-}
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-
-// Delete Products 
-
-exports.deleteProduct = async (req, res , next) => {
-    let product = await Product.findById(req.params.id);
-    if(!product){
-        return res.status(500).json({
-            success: false,
-            message: "Product not found"
-        })
-    }
-await product.deleteOne();
-res.status(200).json({
+  res.status(200).json({
     success: true,
-    message: "Product deleted successfully"
-    })
+    message: "Product updated successfully",
+    product,
+  });
+};
 
-}
+// Delete Products
+
+exports.deleteProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+  await product.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: "Product deleted successfully",
+  });
+};
--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -8,6 +8,6 @@ module.exports = (err,  req , res, next) => {
 
     res.status(err.statusCode).json({
         success:false,
-        error :err
+        message :err.message,
     })
 };
