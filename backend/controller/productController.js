db545f8a0a5e5d8efe449d88d3cade1f662609c9
optanwar <optanwar10@gmail.com>
2025-01-21 02:33:26 +0530

    create product module

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -26,3 +26,46 @@ exports.getAllProducts = async (req, res, next)=>{
         products
     })
 }
+
+
+
+// Update product --Admin
+
+exports.updateProduct = async (req, res, next) =>{
+    let product = await Product.findById(req.params.id);
+   
+    if(!product){
+        return res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body,  {
+        new: true,
+        runValidators: true
+      });
+
+    res.status(200).json({
+        success: true, 
+        message: "Product updated successfully",
+        product})
+}
+
+
+// Delete Products 
+
+exports.deleteProduct = async (req, res , next) => {
+    let product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+await product.deleteOne();
+res.status(200).json({
+    success: true,
+    message: "Product deleted successfully"
+    })
+
+}
