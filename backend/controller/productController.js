d5e716f2e75db685ffb775a07af68fabbed93d57
optanwar <optanwar10@gmail.com>
2025-01-21 01:38:25 +0530

    create product module

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,5 +1,28 @@
-exports.getAllProducts = (req, res, next)=>{
- res.status(200).json({
-    message:"Products are available"
- })
+const Product = require("../module/productModule")
+
+
+
+
+// Create Products --Admin
+exports.createProduct =async ( req, res, next ) =>{
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        message: "Product created successfully",
+        product
+    })
+}
+
+
+// Get all Products
+
+exports.getAllProducts = async (req, res, next)=>{
+    const products = await Product.find();
+
+    res.status(200).json({
+        success: true,
+        message: "Products retrieved successfully",
+        products
+    })
 }
--- backend/module/productModule.js
+++ backend/module/productModule.js
@@ -21,7 +21,7 @@ const productSchema = mongoose.Schema({
         type:Number,
         default:0
     },
-    image:[
+    images:[
         
         {
             public_id:{
