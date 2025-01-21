24d7e73ca6e988a5be6cfa5b16233ec7a717b4b4
optanwar <optanwar10@gmail.com>
2025-01-22 03:17:32 +0530

    create protect route

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,10 +1,24 @@
+const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("./catchAsyncErrors");
-
-
-const isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
-    if (!req.user) {
+const jwt = require("jsonwebtoken");
+const User = require("../module/userModel");
+exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
+    const {token} = req.cookies;
+    if (!token) {
         return next(new ErrorHandler("Login first to access this resource.", 401));
     }
-    next();
+const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+req.user = await User.findById(decodedData.id);
+
+next();
+});
+
+exports.authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(new ErrorHandler(`Role (${req.user.role}) is not allowed to access this resource`, 403));
+        }
+        next();
+    }
 }
-);
