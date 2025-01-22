94b63eb28dcb9ae2a4215ee0825668772ddea23d
optanwar <optanwar10@gmail.com>
2025-01-22 17:40:54 +0530

    add nodemailer

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -15,6 +15,26 @@ let message = err.message || "Internal Server Error";
       
     }
 
+    // mongoose duplicate key error
+    if(err.code === 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+        err = new ErrorHandler(message,400)
+    }
+
+    // wrong jwt error
+    if(err.name === 'JsonWebTokenError'){
+        const message = 'Json Web Token is invalid. Try again!!!';
+        err = new ErrorHandler(message,400)
+    }
+
+    // wrong jwt expired error
+    if(err.name === 'TokenExpiredError'){
+        const message = 'Json Web Token is expired. Try again!!!';
+        err = new ErrorHandler(message,400)
+    }
+
+
+
 
     res.status(statusCode).json({
         success:false,
