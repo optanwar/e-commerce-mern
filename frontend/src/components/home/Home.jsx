477cc3b87b7d588db771f2ac2df9bcccd9a6f608
optanwar <optanwar10@gmail.com>
2025-01-24 01:37:59 +0530

    add products api home

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import HeroSection from './HeroSection'
 import ProductSection from './ProductSection'
 import TestimonialSection from './Testimonial'
+import MetaData from '../../layout/MetaData'
 
 const Home = () => {
   return (
  <>
+ <MetaData title={'Buy Best Products Online'}/>
  <HeroSection/>
  <ProductSection/>
  <TestimonialSection/>
