28d25e9223a8fb48cb924d55c2f12b03a7b332d2
optanwar <optanwar10@gmail.com>
2025-01-24 03:08:04 +0530

    add products details section

--- frontend/src/slices/productSlice.js
+++ frontend/src/slices/productSlice.js
@@ -7,18 +7,28 @@ export const fetchProducts = createAsyncThunk('products/fetchProducts', async ()
   const response = await axiosInstance.get('/products'); // Your API endpoint for products
   return response.data;
 });
+// Async thunk for fetching product details by ID
+export const fetchProductDetails = createAsyncThunk(
+  'products/fetchProductDetails',
+  async (productId) => {
+    const response = await axiosInstance.get(`/product/${productId}`); // Replace with your API endpoint for product details
+    return response.data;
+  }
+);
 
 const productSlice = createSlice({
   name: 'products',
   initialState: {
     products: [],
+    productDetails: null,
     loading: false,
     error: null,
   },
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchProducts.pending, (state) => {
+       // For fetching all products
+       .addCase(fetchProducts.pending, (state) => {
         state.loading = true;
         state.error = null;
       })
@@ -27,6 +37,20 @@ const productSlice = createSlice({
         state.products = action.payload;
       })
       .addCase(fetchProducts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+
+      // For fetching product details
+      .addCase(fetchProductDetails.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchProductDetails.fulfilled, (state, action) => {
+        state.loading = false;
+        state.productDetails = action.payload;
+      })
+      .addCase(fetchProductDetails.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error.message;
       });
