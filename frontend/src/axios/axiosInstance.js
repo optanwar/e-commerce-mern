eda8136c41d4c7b18cdbee1ae8b217fbdc8adb65
optanwar <optanwar10@gmail.com>
2025-01-24 01:26:14 +0530

    add products api home

--- frontend/src/axios/axiosInstance.js
+++ frontend/src/axios/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://api.example.com', // Replace with your base URL
+  baseURL: 'http://localhost:4000/api/v1', // Replace with your base URL
 });
 
 // Add interceptors for error handling globally
