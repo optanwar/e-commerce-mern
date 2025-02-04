c2e3083ddac90074fce27c5e1fa1973c72a97e0e
optanwar <optanwar10@gmail.com>
2025-02-05 01:01:57 +0530

    fixed redux issue

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,24 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // You can change to sessionStorage if preferred
+import storage from 'redux-persist/lib/storage'; // Uses localStorage by default
+
 import productReducer from '../slices/productSlice';
 import userReducer from '../slices/authSlice';
 
-
+// Persist config: Only persist the 'user' slice (authentication)
 const persistConfig = {
   key: 'root',
   storage,
+  whitelist: ['user'], // Persist 'user' slice only
 };
 
-const persistedProductReducer = persistReducer(persistConfig, productReducer);
-
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-    products: persistedProductReducer,
-  },
+// Combine all reducers
+const rootReducer = combineReducers({
+  user: userReducer, // Auth state will now be persisted
+  products: productReducer, // Product state is NOT persisted
 });
 
+// Apply persistReducer to root reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create Redux store
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Prevent errors from redux-persist
+    }),
+});
+
+// Persistor for persisting the store
 const persistor = persistStore(store);
 
 export { store, persistor };
