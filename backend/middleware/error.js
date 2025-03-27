a5030e068de0cbfa3e914a2918759522134c5a0a
optanwar <optanwar10@gmail.com>
2025-03-27 12:32:05 +0530

    handle type of errors

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -5,6 +5,17 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || 'Internal Server Error';
 
+
+
+    // Wrong Mongoose Object ID Error
+    if (err.name === 'CastError') {
+        const message = 'Resource not found. Please check the ID. Invalid: ' + err.path;
+        err = new ErrorHandler(message, 400);
+    }
+
+
+
+
     res.status(err.statusCode).json({
         success: false,      
         error: err.stack,
