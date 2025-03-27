a01ae54951245fe269df721165e4b718601affe7
optanwar <optanwar10@gmail.com>
2025-03-27 14:51:04 +0530

    add filter in backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require('../models/productModel');
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middleware/catchAsyncErrors');
+const ApiFeatures = require('../routes/apiFeatures');
 
 // Create new product => Admin
 
@@ -18,7 +19,11 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 // Get all products 
 exports.getAllProducts =catchAsyncErrors( async (req, res) => {
-  const products = await Product.find();
+
+  const resultPerPage = 5;
+  const productCount = await Product.countDocuments();
+  const apiFeature = new ApiFeatures( Product.find(), req.query).search().filter().pagination(resultPerPage);
+  const products = await apiFeature.query;  
   res.status(200).json({
     success: true,
     message: 'Products get successfully!',
