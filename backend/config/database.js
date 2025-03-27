d6af42c5a7c19a099a4b736d816b644d8bae56dd
optanwar <optanwar10@gmail.com>
2025-03-27 12:23:04 +0530

    handle unhandled promise rejection

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const connectDatabase = () => {
     mongoose.connect(process.env.DB_LOCAL_URI).then(data => {
         console.log(`MongoDB Database connected with HOST: ${data.connection.host}`)
-    }).catch(err => {
-        console.log(err);
-    })
+    });
 }
 module.exports = connectDatabase;
--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,17 @@ dotenv.config({ path: "backend/config/config.env" });
 // Connecting to database
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on port http://localhost:${process.env.PORT}`);
+});
+
+
+// Unhandled Promise rejection
+
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
