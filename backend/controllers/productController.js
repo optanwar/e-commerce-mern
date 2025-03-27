b82df61c5dec7ee427ea7730eae9708144240e3a
optanwar <optanwar10@gmail.com>
2025-03-27 09:05:44 +0530

    all product api

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,26 @@
+const Product = require('../models/productModel');
+
+// Create new product => Admin
+
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+
+  res.status(201).json({
+    status: 'success',
+    message: 'Products is created!',
+    product
+  });
+
+};
+
+// Get all products 
 exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
   res.status(200).json({
     status: 'success',
-    message: 'This route is not yet defined!'
+    message: 'Products get successfully!',
+    products
   });
 
 }
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,14 @@
-const app = require('./app');
-const dotenv = require('dotenv');
+const app = require("./app");
+const dotenv = require("dotenv");
 
-const connectDatabase = require('./config/database');
+const connectDatabase = require("./config/database");
 
 // Setting up config file
-dotenv.config({ path: 'backend/config/config.env' });
-
+dotenv.config({ path: "backend/config/config.env" });
 
 // Connecting to database
 connectDatabase();
 
-
-app.listen(process.env.PORT , () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on port http://localhost:${process.env.PORT}`);
-}   );
-
+});
