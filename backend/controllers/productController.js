51e93ac953e0bef6d448ff219b60f7e736ae3b94
optanwar <optanwar10@gmail.com>
2025-03-27 09:48:22 +0530

    complete produts route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,7 +7,7 @@ exports.createProduct = async (req, res, next) => {
 
 
   res.status(201).json({
-    status: 'success',
+    success: true,
     message: 'Products is created!',
     product
   });
@@ -18,9 +18,71 @@ exports.createProduct = async (req, res, next) => {
 exports.getAllProducts = async (req, res) => {
   const products = await Product.find();
   res.status(200).json({
-    status: 'success',
+    success: true,
     message: 'Products get successfully!',
     products
   });
 
 }
+
+
+// Get single product details
+
+exports.getProductDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found'
+    });
+  }
+  res.status(200).json({
+    success: true,
+    message: 'Product details get successfully!',
+    product
+  });
+}
+
+
+
+
+// Update product => Admin
+
+exports.updateProduct = async ( req, res , next ) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found'
+    });
+  }
+  
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false
+  });
+
+  res.status(200).json({
+    success: true,
+    message: 'Product updated successfully!',
+    product
+  });
+}
+
+// Delete product => Admin
+
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found'
+    });
+  }
+  await product.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: 'Product is deleted successfully!'
+  });
+}   
