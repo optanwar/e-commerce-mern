2191ecd5c07fec1e97f90e5592e49beb586e77c2
optanwar <optanwar10@gmail.com>
2025-04-15 17:59:08 +0530

    add product details

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -21,7 +21,7 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 // Get all products 
 exports.getAllProducts =catchAsyncErrors( async (req, res) => {
 
-  const resultPerPage = 5;
+  const resultPerPage = 8;
   const productCount = await Product.countDocuments();
   const apiFeature = new ApiFeatures( Product.find(), req.query).search().filter().pagination(resultPerPage);
   const products = await apiFeature.query;  
