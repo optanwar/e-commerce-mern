d0942b629c3defdb9274d0f0c94b29bbd32d03e8
optanwar <optanwar10@gmail.com>
2025-04-15 12:20:05 +0530

    solve cors error

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,22 @@
+
+
+
 const express = require('express')
 const app = express()
+
 const cookieParser = require('cookie-parser');
 const errorMiddleware = require('./middleware/error')
+const cors = require('cors');
 
 
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(cors({
+    origin: 'http://localhost:5173', // Replace with your frontend origin
+    credentials: true,
+  }));
 
 
 
