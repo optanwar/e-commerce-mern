27db4a14e5958b47f80cd0795c4737213bded137
optanwar <optanwar10@gmail.com>
2025-04-15 11:20:55 +0530

    implement react halmet

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -5,10 +5,12 @@ import About from './About'
 import Testimonial from './Testimonial'
 import Feedback from './Feedback'
 import FAQ from './FAQ';
+import MetaData from '../../layout/MetaData'
 
 const Home = () => {
   return (
    <>
+  <MetaData title="website name title" />
    <HeroSection/>
    <About/>
    <ProductSection/>
