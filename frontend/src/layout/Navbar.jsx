de7926c63e5020e81d681a64882894e2e24c4e5e
optanwar <optanwar10@gmail.com>
2025-04-20 00:28:49 +0530

    add contact setion routes

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
             <li><Link to={`/products`} className="text-gray-700 hover:text-primary">Products</Link></li>
             <li><Link to={`/about`}  className="text-gray-700 hover:text-primary">About</Link></li>
             <li><a href="#services" className="text-gray-700 hover:text-primary">Blogs</a></li>
-            <li><a href="#contact" className="text-gray-700 hover:text-primary">Contact</a></li>
+            <li><Link to={`/contact-us`} className="text-gray-700 hover:text-primary">Contact</Link></li>
           </ul>
 
           <div className='hidden md:flex items-center space-x-4'>
