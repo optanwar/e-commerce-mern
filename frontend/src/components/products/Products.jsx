222a232d97bbf95cd8e9f3849eada02854ce8671
optanwar <optanwar10@gmail.com>
2025-04-19 14:43:43 +0530

    add products section

--- frontend/src/components/products/Products.jsx
+++ frontend/src/components/products/Products.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <div className='bg-gray-100 '>
+      <div className='container mx-auto bg-red-600'>
+
+        <div className='flex flex-col md:flex-row  md:justify-start md:items-center '>
+          <div className=''>
+            Filter
+          </div>
+          <div className=''>
+          Our Products
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
